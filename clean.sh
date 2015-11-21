3badb8846fdfb43ec3cd2f37057eb255b33f23e8
RazorSharp <kirananto@gmail.com>
2015-11-21 06:39:03 +0530

    buildscripts : update it for my pc

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 KERNEL_DIR=$PWD
-export CROSS_COMPILE="$MODULES_DIR/../../../Toolchains/aarch64-linux-android-5.2-kernel/bin/aarch64-linux-android-"
+export CROSS_COMPILE="$KERNEL_DIR/../Toolchains/arm-eabi-4.9/bin/arm-eabi-"
 export ARCH=arm64
 export SUBARCH=arm64
 make clean
